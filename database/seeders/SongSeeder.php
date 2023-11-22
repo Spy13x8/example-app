d41acc238ab8795a7fdd9417a58560c1863eb5f4
Spy13x8 <sahilyadao13@gmail.com>
2023-11-23 01:13:27 +0530

    Create Song database model

---
+++ database/seeders/SongSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+use App\Models\Song;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class SongSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+
+            Song::create([
+                'artist' => 'Eminem',
+                'title' => 'Stan',
+            ]);
+    
+            Song::create([
+                'artist' => 'A P Dhillon',
+                'title' => 'With You',
+            ]);
+    
+        }
+    }
