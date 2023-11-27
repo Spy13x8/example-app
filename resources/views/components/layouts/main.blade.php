fdffa01715450b9aecebb030af15f5da778ffcf3
Spy13x8 <sahilyadao13@gmail.com>
2023-11-27 23:35:34 +0530

    Create a new component to render songs list

--- resources/views/components/layouts/main.blade.php
+++ resources/views/components/layouts/main.blade.php
@@ -1,96 +1,126 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Spotify-like Playlist</title>
-    <link rel="stylesheet" href="{{ asset('css/layouts/main.css') }}">
-</head>
-<body>
+    <title>Playlist Page</title>
+    <style>
+        body {
+            font-family: 'Arial', sans-serif;
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
 
-    <!-- Header Section -->
-    <header>
-        <div class="playlist-name">
-            Playlist Name
-            <div class="search-form">
-                <form>
-                    <input type="text" class="search-input" placeholder="Search song...">
-                    <button type="submit" class="button">Search</button>
-                </form>
-            </div>
-        </div>
-        <div>
-            <a href="#" class="button">Edit Playlist</a>
-            <a href="#" class="button">New Playlist</a>
-        </div>
+        header {
+            background-color: #333;
+            color: white;
+            padding: 10px;
+            text-align: center;
+            
+        }
+
+        main {
+            display: grid;
+            grid-template-columns: 1fr 3fr;
+            grid-gap: 20px;
+            padding: 20px;
+        }
+
+        aside {
+            background-color: #f4f4f4;
+            padding: 10px;
+        }
+
+        section {
+            width: 100%;
+        }
+
+        section div {
+            display: flex;
+            justify-content: space-between;
+            padding: 10px;
+            border: 1px solid #ddd;
+            margin-bottom: 5px;
+        }
+
+        section div:nth-child(odd) {
+            background-color: #f2f2f2;
+        }
+
+        footer {
+            background-color: #333;
+            color: white;
+            padding: 10px;
+            text-align: center;
+            position: fixed;
+            bottom: 0;
+            width: 100%;
+        }
+        
+        .b1, .b2{
+            background-color: green;
+            height: 40px;
+            width: 200px;
+            margin: 12px;
+        }
+        .nav{
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+        }
+
+    </style>
+</head>
+
+<body>
+    <header class="nav">
+        <h1 class="playlistName">Playlist Name</h1>
+
+        <button class="b1">Edit Playlist</button>
+        <button class="b2">New Playlist</button>
     </header>
 
-    <!-- Sidebar Section -->
-    <aside>
-        <h2>Saved Playlists</h2>
-        <ul>
-            <li>Playlist 1</li>
-            <li>Playlist 2</li>
-            <!-- Add more playlists as needed -->
-        </ul>
-    </aside>
-
-    <!-- Main Content Section -->
     <main>
-        <!-- Playlist Item -->
-        <div class="playlist">
-            <div class="playlist-details">
-                <div class="playlist-name">Playlist 1</div>
-                <div class="playlist-songs">
-                    <!-- Sample Songs -->
-                    <div class="song">
-                        <div class="song-title">Stairway to Heaven</div>
-                        <div class="song-details">
-                            <span>Artist: Led Zeppelin</span>
-                            <span>Album: Sample Album</span>
-                            <span>Duration: 8:05</span>
-                        </div>
-                    </div>
+        <aside>
+            <h2>Saved Playlists</h2>
+            <ul>
+                <li>Playlist 1</li>
+                <li>Playlist 2</li>
+                <li>Playlist 3</li>
+            </ul>
+        </aside>
 
-                    <div class="song">
-                        <div class="song-title">Hotel California</div>
-                        <div class="song-details">
-                            <span>Artist: Eagles</span>
-                            <span>Album: Hotel California</span>
-                            <span>Duration: 6:31</span>
-                        </div>
-                    </div>
-
-                    <div class="song">
-                        <div class="song-title">Dear Mr. Fantasy</div>
-                        <div class="song-details">
-                            <span>Artist: Traffic</span>
-                            <span>Album: Sample Album</span>
-                            <span>Duration: 5:34</span>
-                        </div>
-                    </div>
-
-                    <div class="song">
-                        <div class="song-title">California Dreamin'</div>
-                        <div class="song-details">
-                            <span>Artist: The Mamas and the Papas</span>
-                            <span>Album: Sample Album</span>
-                            <span>Duration: 2:40</span>
-                        </div>
-                    </div>
-                    <!-- End Sample Songs -->
-                </div>
+        <section>
+            <div>
+                <div>Serial No</div>
+                <div>Song Title</div>
+                <div>Artist Name</div>
+                <div>Album Name</div>
+                <div>Duration</div>
             </div>
-        </div>
-
-        <!-- Add more playlists as needed -->
-
+            <div>
+                <div>1</div>
+                <div>Song 1</div>
+                <div>Artist 1</div>
+                <div>Album 1</div>
+                <div>3:30</div>
+            </div>
+            <div>
+                <div>2</div>
+                <div>Song 2</div>
+                <div>Artist 2</div>
+                <div>Album 2</div>
+                <div>4:15</div>
+            </div>
+            <!-- Add more rows as needed -->
+        </section>
     </main>
 
-    <!-- Footer Section -->
     <footer>
         <p>&copy; 2023 Playlist App | <a href="#">About</a> | <a href="#">Contact</a></p>
     </footer>
-
 </body>
+
 </html>
